f5ec4b8b50f8f16ea32b3a349b233c8c36b2e103
Greg Gzik <gregorygzik@gmail.com>
2018-11-19 17:05:18 -0800

    Began template to AST creation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import * as babylon from "babylon";
+import * as esprima from "esprima";
+import * as fs from "fs";
+
+const input_code = fs.readFileSync("./src/sublist-operator.ejs", { encoding: "UTF-8"});
+const expected_output_code = fs.readFileSync("./src/sublist-operator.js", { encoding: "UTF-8"});
+
+const precurser_to_ast = esprima.tokenize(input_code);
+const expected_ast = babylon.parse(expected_output_code);
+
+console.log(precurser_to_ast );
+
+// SEE ALSO Acorn Parser and its plugins
 
 /**
  * TODO:
